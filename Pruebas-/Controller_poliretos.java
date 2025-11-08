16651c78e438da3232fe45f90c43425dfd2866cc
Matias Quinchiguano <matercra777@gmail.com>
2025-11-08 08:25:05 -0500

    a

--- Pruebas-/Controller_poliretos.java
+++ Pruebas-/Controller_poliretos.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class   Controller_poliretos {
+    Scanner IngresarValores = new Scanner(System.in);
         public void iniciar(){ 
+            String valoresGenericos;
+            Integer operaciones;
             final String ROJO = "\u001B[31m";
             final String RESET = "\u001B[0m";
             final String VERDE = "\u001B[32m";
@@ -13,6 +18,37 @@ public class   Controller_poliretos {
             System.out.printf("%54s%n", "Bienvenido a Poliretos.");
             System.out.println();
             System.out.printf("%63s%n", VERDE + "====== MENU DE OPERACIONES =====");
+
+            do{ 
+            valoresGenericos = IngresarValores.nextLine(); 
+            operaciones =Integer.parseInt(valoresGenericos);
+            }while(operaciones <1 || operaciones >8);    
+            switch (operaciones) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 8:
+
+                    break;
+            }
         }
 
 }
