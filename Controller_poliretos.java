2d070b09711ab0733cf53ac72c40402282cf0735
Matias Quinchiguano <matercra777@gmail.com>
2025-11-08 07:59:07 -0500

    a

--- Controller_poliretos.java
+++ Controller_poliretos.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class   Controller_poliretos {
+            Scanner ingresoDatos = new Scanner(System.in);
         public void iniciar(){ 
-            
+
         }
 
 }
