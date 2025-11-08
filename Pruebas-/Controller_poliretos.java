99828f521c1635c5325dc595f3ffc95b49ade6dd
C-b4s <szuniga046@gmail.com>
2025-11-08 08:26:07 -0500

    fdfsfdsf

--- Pruebas-/Controller_poliretos.java
+++ Pruebas-/Controller_poliretos.java
@@ -4,15 +4,22 @@ public class   Controller_poliretos {
             final String RESET = "\u001B[0m";
             final String VERDE = "\u001B[32m";
 
-            String titulo = "============== POLIRETOS ==============";
-            int ancho = 80; // ancho promedio del terminal
-
-            int espacios = (ancho - titulo.length()) / 2;
-
+            String [] secciones = {"1. Series numericas", "2. Series de caracteres", "3. Figuras", "4. Cadenas de caracteres", "5. Arrays", "6. Loading", "7. Recursion", "8. Grafos y automatas"};
+            
+        do{
             System.out.printf("%70s%n", ROJO + "============== POLIRETOS ==============" + RESET);
             System.out.printf("%54s%n", "Bienvenido a Poliretos.");
             System.out.println();
             System.out.printf("%63s%n", VERDE + "====== MENU DE OPERACIONES =====");
+
+            for (int i = 0; i < secciones.length; i++){
+                System.out.println("|");
+                System.out.println(secciones[i]);
+                
+            }
+
+        }while   
+           
         }
 
 }
