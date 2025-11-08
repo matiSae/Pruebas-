977df91d450c0214e35d342c8cd369ba1b68c2f1
Matias Quinchiguano <matercra777@gmail.com>
2025-11-08 07:29:43 -0500

    a

---
+++ AppJava.java
@@ -0,0 +1,7 @@
+public class AppJava {
+    public static void main(String[] args) throws InterruptedException {
+        Controller_poliretos controller= new Controller_poliretos();
+        controller.iniciar();
+    }
+    
+}
